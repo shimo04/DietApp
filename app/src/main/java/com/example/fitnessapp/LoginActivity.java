66de0dcd538fa1829b47bc888f038fc1c8a5c90f
shimo04 <mejrishayma1@gmail.com>
2022-05-30 21:22:32 +0100

    Sec Push Login

--- app/src/main/java/com/example/fitnessapp/LoginActivity.java
+++ app/src/main/java/com/example/fitnessapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.fitnessapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -15,9 +16,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -44,7 +50,7 @@ public class LoginActivity extends AppCompatActivity
         Auth = FirebaseAuth.getInstance();
         //Login.setOnClickListener(new View.OnClickListener()
         //                               {
-                                           @Override
+                                  //         @Override
           //                                 public void onClick (View view)
            //                            {
           //                                 startActivity(new Intent(LoginActivity.this,MainActivity.class));
@@ -54,23 +60,56 @@ public class LoginActivity extends AppCompatActivity
 
             //                   );
 
-        Login.setOnClickListener(new View.OnClickListener() {
-                                     @Override
-                                     public void onClick (View view)
-                                     {
-                                         ProgressDialog pd = new ProgressDialog(LoginActivity.this);
-                                         pd.setMessage("please wait");
-                                         pd.show();
+        Login.setOnClickListener(new View.OnClickListener()
+                                           {
+                                            @Override
+                                            public void onClick (View view)
+                                            {
+                                                ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+                                                pd.setMessage("please wait");
+                                                pd.show();
 
-                                         String strEmail = Email.getText().toString();
-                                         String strPass = Password.getText().toString();
-                                         if(TextUtils.isEmpty(strEmail) || (TextUtils.isEmpty(strPass))
-                                         {
-                                             Toast.makeText(LoginActivity.this, "all feilds are required", Toast.LENGTH_SHORT).show();
-                                         }
-                                         else
-                                         {
-                                             Auth.signInWithEmailAndPassword(strEmail,strPass).addOnCanceledListener(LoginActivity.this, new OnCompleteListener<AuthResult>())
+                                                String strEmail = Email.getText().toString();
+                                                String strPass = Password.getText().toString();
+                                                if(TextUtils.isEmpty(strEmail) || TextUtils.isEmpty(strPass))
+                                                   {Toast.makeText(LoginActivity.this, "all feilds are required", Toast.LENGTH_SHORT).show();}
+                                                else
+                                                   {
+                                                      Auth.signInWithEmailAndPassword(strEmail,strPass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>()
+                                                     {
+                                                         @Override
+                                                         public void onComplete(@NonNull Task<AuthResult> task)
+                                                         {
+                                                             if (task.isSuccessful())
+                                                             {
+                                                                 DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(Auth.getCurrentUser().getUid());
+                                                                 reference.addValueEventListener(new ValueEventListener()
+                                                                     {
+                                                                     @Override
+                                                                     public void onDataChange(@NonNull DataSnapshot snapshot)
+                                                                     {
+                                                                         pd.dismiss();
+                                                                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                                         startActivity(intent);
+                                                                         finish();
+                                                                     }
+
+                                                                     @Override
+                                                                     public void onCancelled(@NonNull DatabaseError error)
+                                                                     {
+                                                                         pd.dismiss();
+                                                                     }
+                                                                 });
+                                                             }
+                                                             else
+                                                             {
+                                                                 pd.dismiss();
+                                                                 Toast.makeText(LoginActivity.this,"Authentification failed", Toast.LENGTH_SHORT).show();
+                                                             }
+                                                         }
+                                                     }
+                                             );
                                          }
                                      }
 
